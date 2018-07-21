3712d18bc2a57350038ce9e9d83004b57860396f
fury lynx <furylynx@users.noreply.github.com>
2018-07-21 18:36:33 +0200

    Update travis-linux-install.sh

--- Tools/travis-linux-install.sh
+++ Tools/travis-linux-install.sh
@@ -46,8 +46,10 @@ case "$SWIGLANG" in
 					# curl -sL https://deb.nodesource.com/setup_${VER}.x | sudo -E bash -
 					# travis_retry sudo apt-get install -y nodejs rlwrap
 					travis_retry wget -qO- https://raw.githubusercontent.com/xtuple/nvm/master/install.sh | sudo bash
-					travis_retry sudo nvm install ${VER}
-					sudo nvm use ${VER}
+					travis_retry nvm install ${VER}
+					nvm use ${VER}
+					npm --version
+					node --version
 					travis_retry npm install -g node-gyp
 				fi
 				;;
