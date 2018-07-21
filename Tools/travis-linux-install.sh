d802f317c8382b3417f8db14317d3d46893b07d5
fury lynx <furylynx@users.noreply.github.com>
2018-07-21 18:40:40 +0200

    Update travis-linux-install.sh

--- Tools/travis-linux-install.sh
+++ Tools/travis-linux-install.sh
@@ -40,13 +40,8 @@ case "$SWIGLANG" in
 				if [[ -z "$VER" ]]; then
 					travis_retry sudo apt-get install -qq nodejs node-gyp
 				else
-					# travis_retry curl -sL https://deb.nodesource.com/setup_${VER}.x | sudo -E bash -
-					# travis_retry sudo apt-get install -y nodejs
-					# travis_retry npm install -g node-gyp
-					# curl -sL https://deb.nodesource.com/setup_${VER}.x | sudo -E bash -
-					# travis_retry sudo apt-get install -y nodejs rlwrap
 					travis_retry wget -qO- https://raw.githubusercontent.com/xtuple/nvm/master/install.sh | sudo bash
-					travis_retry nvm install ${VER}
+					travis_retry sudo nvm install ${VER}
 					nvm use ${VER}
 					npm --version
 					node --version
